74108ad204843041cd6db3cec05d0c1c5eb1ad83
YashGarg220 <yash.garg2702@gmail.com>
2022-11-06 11:11:52 +0530

    earned about statements, expressions, null, undefined

--- statements-expressions.js
+++ statements-expressions.js
@@ -1,23 +1,7 @@
-// function pesto (a, b){
-//     return
-//     a+b;
-// }
+function pesto (a, b){
+    return
+    a+b;
+}
 
-// console.log(pesto(3,4));
+console.log(pesto(3,4));
 
-//undefined and null
-
-// var x;
-// console.log(x);
-
-
-// function pesto (a,b){
-//     console.log('hi');
-//     console.log(a);
-//     console.log(b);
-// }
-// pesto(1);
-
-const obj = {};
-console.log(obj.name)
-;
---
+++ undefined.js
@@ -0,0 +1,16 @@
+//undefined 
+
+var x;
+console.log(x);
+
+
+function pesto (a,b){
+    console.log('hi');
+    console.log(a);
+    console.log(b);
+}
+pesto(1);
+
+const obj = {};
+console.log(obj.name)
+;
